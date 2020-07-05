3e7c467ee075b14ecc41d06f5c56e2742166a12e
FinchyG <lagaunt120@yahoo.com>
2020-07-05 21:41:54 +0100

    Second example complete

---
+++ StoreIndexer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StoreIndexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StoreLocations StoreArr = new StoreLocations();
+
+            StoreArr[0] = "Halifax";
+            StoreArr[1] = "Leeds";
+            StoreArr[2] = "Bradford";
+            StoreArr[3] = "Huddersfield";
+
+            for (int i = 0; i < 4; i++)
+                Console.WriteLine(StoreArr[i]);
+        }
+    }
+}
---
+++ StoreIndexer/StoreLocations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StoreIndexer
+{
+    class StoreLocations
+    {
+        private string[] StoreArr = new string[4];
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= StoreArr.Length)
+                    throw new IndexOutOfRangeException("Index out of range.");
+
+                return StoreArr[index];
+            }
+            set
+            {
+                if(index < 0 || index >= StoreArr.Length)
+                    throw new IndexOutOfRangeException("Index out of range.");
+
+                StoreArr[index] = value;
+            }
+        }
+    }
+}
