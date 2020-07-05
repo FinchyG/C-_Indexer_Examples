83471f6d0408fefcef9df1200711270308dbe1cc
FinchyG <lagaunt120@yahoo.com>
2020-07-05 21:14:41 +0100

    One example complete

---
+++ IndexerDemo/MyArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndexerDemo
+{
+    class MyArray
+    {
+        int _lowerBound;
+        int _upperBound;
+        string[] _items;
+
+        public MyArray(int lowerBound, int upperBound)
+        {
+            _lowerBound = lowerBound;
+            _upperBound = upperBound;
+            _items = new string[1 + upperBound - lowerBound];
+        }
+
+        public string this[int index]
+        {
+            get { return _items[index - _lowerBound]; }
+            set { _items[index - _lowerBound] = value; }
+        }
+    }
+}
---
+++ IndexerDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IndexerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyArray fruits = new MyArray(1, 4);
+            fruits[1] = "Apple";
+            fruits[2] = "Orange";
+            fruits[3] = "Banana";
+            fruits[4] = "Pineapple";
+
+            Console.WriteLine(fruits[1]);
+            Console.WriteLine(fruits[4]);
+        }
+    }
+}
