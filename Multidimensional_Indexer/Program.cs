7c89e91d31843de55640eed4878c2e6b2beb0279
FinchyG <lagaunt120@yahoo.com>
2020-07-07 19:21:04 +0100

    Third example complete

---
+++ Multidimensional_Indexer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Multidimensional_Indexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimesTables timesTables = new TimesTables();
+
+            timesTables[0, 0] = 2;
+            timesTables[0, 1] = 4;
+            timesTables[0, 2] = 6;
+
+            timesTables[1, 0] = 3;
+            timesTables[1, 1] = 6;
+            timesTables[1, 2] = 9;
+
+            timesTables[2, 0] = 4;
+            timesTables[2, 1] = 8;
+            timesTables[2, 2] = 12;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write("{0}\t", timesTables[i, j]);
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
---
+++ Multidimensional_Indexer/TimesTables.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Multidimensional_Indexer
+{
+    class TimesTables
+    {
+        private int[,] productsArr = new int[3, 3];
+        
+        public int this[int index1, int index2]
+        {
+            get
+            {
+                return productsArr[index1, index2];
+            }
+
+            set
+            {
+                productsArr[index1, index2] = value;
+            }
+        }
+
+
+    }
+}
